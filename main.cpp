e66c75ae5af2be6cf5c778214d0d4d42372aa5dc
zhouqiang <2902511578@qq.com>
2024-03-08 17:35:19 +0800

    hello world

---
+++ main.cpp
@@ -0,0 +1,12 @@
+/*
+ * pre-process（预处理）：发生在真正编译之前
+ * # 开头为预处理指令，编译器做的第一件事是预处理所有预处理指令
+ *
+ * delcaration（声明）：
+ */
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
