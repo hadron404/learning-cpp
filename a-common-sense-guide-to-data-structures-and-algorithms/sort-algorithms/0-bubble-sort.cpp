84c3a3eb27b4c41743a98c0d196bf552f40fe6bc
zhouqiang <2902511578@qq.com>
2024-03-21 17:50:40 +0800

    Reading Record: A common-sense guide to data structures and algorithms
    implement bubble sort and cocktail shaker sort algorithms

--- a-common-sense-guide-to-data-structures-and-algorithms/sort-algorithms/0-bubble-sort.cpp
+++ a-common-sense-guide-to-data-structures-and-algorithms/sort-algorithms/0-bubble-sort.cpp
@@ -1,3 +1,57 @@
-//
-// Created by HP on 2024/3/20.
-//
+#include <iostream>
+
+
+template<typename T>
+int getArrayLength(T &array) {
+    return sizeof(array) / sizeof(array[0]);
+}
+
+template<typename T>
+void printArray(T &array) {
+    using namespace std;
+    int length = getArrayLength(array);
+    for (int i = 0; i < length; ++i) {
+        cout << array[i] << " ";
+    }
+    cout << endl;
+}
+
+/*
+ * 数组的初始化
+ * 数组作为函数的参数
+ * 数组的长度
+ * 遍历数组
+ * 数组元素的赋值
+ * 打印数组
+ *
+ */
+template<typename T>
+void sort(T &array) {
+    int length = getArrayLength(array);
+    std::cout << "length of an array: " << length << std::endl;
+    bool sorted = false;
+    while (!sorted) {
+        sorted = true;
+        int endIdx = length - 1;
+        for (int i = 0; i < endIdx; ++i) {
+            if (array[i] > array[i + 1]) {
+                std::swap(array[i], array[i + 1]);
+                sorted = false;
+            }
+        }
+    }
+}
+
+
+int main() {
+    // 数组的初始化
+    int array[5] = {4, 2, 7, 1, 3};
+    int length = getArrayLength(array);
+    printArray(array);
+    sort(array);
+    // 打印数组
+    std::cout << "final array: ";
+    for (int i = 0; i < length; ++i) {
+        std::cout << array[i];
+    }
+}
---
+++ a-common-sense-guide-to-data-structures-and-algorithms/sort-algorithms/cocktail-shaker-sort.cpp
@@ -0,0 +1,19 @@
+#include  <iostream>
+#include <array>
+
+
+void cocktailShakerSort(std::array<int,10>  , int length) {
+    int left = 0, right = length - 1;
+
+
+    // 变量 variable
+    // 赋值 assign
+    // 迭代
+    // 条件判断
+    // while(left < right)
+}
+
+int main() {
+    int array[5] = {4, 2, 7, 1, 3};
+
+}
