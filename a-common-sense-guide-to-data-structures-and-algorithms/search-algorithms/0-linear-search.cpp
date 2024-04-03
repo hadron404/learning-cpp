f3d758d2319b640f014427d63e04dd8068ede781
zhouqiang <2902511578@qq.com>
2024-04-03 16:30:19 +0800

    Reading Record: A common-sense guide to data structures and algorithms
    implement linear search

--- a-common-sense-guide-to-data-structures-and-algorithms/search-algorithms/0-linear-search.cpp
+++ a-common-sense-guide-to-data-structures-and-algorithms/search-algorithms/0-linear-search.cpp
@@ -1,3 +1,18 @@
-//
-// Created by HP on 2024/3/20.
-//
+int search(int array[], int target, int size) {
+    for (int i = 0; i < size; ++i) {
+        if(array[i] == target)
+            return i;
+        if(array[i] > target)
+            return -1;
+    }
+    return -1;
+}
+
+int main() {
+
+    int arraySorted[] = {};
+    int size = 5;
+    int target = 3;
+    int index = search(arraySorted,target,size);
+
+}
