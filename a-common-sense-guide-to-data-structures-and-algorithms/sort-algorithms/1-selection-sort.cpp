4d6480c7abec053b2acd1960286e1ddc738788c6
zhouqiang <2902511578@qq.com>
2024-04-03 16:52:59 +0800

    Reading Record: A common-sense guide to data structures and algorithms
    implement selection sort

--- a-common-sense-guide-to-data-structures-and-algorithms/sort-algorithms/1-selection-sort.cpp
+++ a-common-sense-guide-to-data-structures-and-algorithms/sort-algorithms/1-selection-sort.cpp
@@ -1,3 +1,25 @@
-//
-// Created by HP on 2024/3/20.
-//
+#include "../tools/printer.h"
+#include <iostream>
+
+void sort(int array[], int size) {
+    int throughpass = 0;
+    while (throughpass < size) {
+        int lowesNumberIdx = throughpass;
+        for (int i = throughpass; i < size; ++i) {
+            if (array[i] < array[lowesNumberIdx])
+                lowesNumberIdx = i;
+        }
+        std::swap(array[lowesNumberIdx], array[throughpass]);
+        ++throughpass;
+        // std::cout << "The throughpass at [" << throughpass << "] :" ;
+        // printArray(array, size);
+    }
+}
+
+
+int main() {
+    int array[] = {4, 2, 7, 1, 3,5,8,5,3,7};
+    int size = 10;
+    sort(array, size);
+    // printArray(array, size);
+}
--- a-common-sense-guide-to-data-structures-and-algorithms/tools/printer.cpp
+++ a-common-sense-guide-to-data-structures-and-algorithms/tools/printer.cpp
@@ -5,7 +5,10 @@
 void printArray(const int array[], const int size) {
     using namespace std;
     for (int i = 0; i < size; ++i) {
-        cout << array[i] << ",";
+        cout << array[i];
+        if (i != size - 1) {
+            cout << ",";
+        }
     }
     cout << endl;
 }
