799052575d35bd8dfd60056c807a10d51382defb
zhouqiang <2902511578@qq.com>
2024-04-03 17:22:07 +0800

    Reading Record: A common-sense guide to data structures and algorithms
    implement insertion sort

--- a-common-sense-guide-to-data-structures-and-algorithms/sort-algorithms/2-insertion-sort.cpp
+++ a-common-sense-guide-to-data-structures-and-algorithms/sort-algorithms/2-insertion-sort.cpp
@@ -1,3 +1,21 @@
-//
-// Created by HP on 2024/3/20.
-//
+#include "../tools/printer.h"
+
+void sort(int array[], int size) {
+    for (int i = 1; i < size; ++i) {
+        int start = i;
+        int temp = array[start];
+        while(start > 0 && array[start-1] > temp) {
+            array[start] = array[start-1];
+            --start;
+        }
+        array[start] = temp;
+    }
+}
+
+
+int main() {
+    int size = 5;
+    int array[size] = {4, 2, 7, 1, 3};
+    sort(array, size);
+    printArray(array, size);
+}
